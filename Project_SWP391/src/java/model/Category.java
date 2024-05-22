aa8bae2948a25cd923a5754526fff0e7c83634dc
anzunnt <67936366+anzunnt@users.noreply.github.com>
2024-05-22 08:41:11 +0700

    add Category
    create Category model

---
+++ Project_SWP391/src/java/model/Category.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import dao.DBContext;
+
+/**
+ *
+ * @author Admin
+ */
+public class Category{
+    
+    private int id;
+    private String name;
+    private String description;
+
+    public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }    
+    
+}
